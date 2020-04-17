4758aa9aee404e2ef352b9dbe97111138d2e4740
boxjan <i@boxjan.li>
2020-04-17 14:01:43 +0800

    delete unused import

--- src/main/java/cn/edu/sdtbu/aop/aspect/TimeCostAspect.java
+++ src/main/java/cn/edu/sdtbu/aop/aspect/TimeCostAspect.java
@@ -4,8 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
